8114ab2a0a0ac6393bf3e7bdee05db59ede391ee
Mark Allibone <mark.allibone@rey-technology.com>
2021-12-02 13:21:46 +0100

    sovles part 2

--- AdventOfCode2021/Scratchpad.fsx
+++ AdventOfCode2021/Scratchpad.fsx
@@ -9,24 +9,30 @@ let getInput day =
     let filename day = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day}.txt")
     File.ReadAllLines(filename day)
     
-
-let positionUpdate (hPos, vPos) (instruction:string[]) =
+let positionUpdatePart2 (hPos, vPos, aim) (instruction:string[]) =
     match instruction[0] with
-    | "forward" -> (hPos + int instruction[1], vPos)
-    | "down" -> (hPos, vPos + int instruction[1])
-    | "up" -> (hPos, vPos - int instruction[1])
-    | _ -> (hPos, vPos)
+    | "forward" -> (hPos + int instruction[1], vPos + aim * int instruction[1], aim)
+    | "down" -> (hPos, vPos, aim + int instruction[1])
+    | "up" -> (hPos, vPos, aim - int instruction[1])
+    | _ -> (hPos, vPos, aim)
 
-let solveDay2 (input:string[]) =
+let positionUpdatePart1 (hPos, vPos, _) (instruction:string[]) =
+    match instruction[0] with
+    | "forward" -> (hPos + int instruction[1], vPos, 0)
+    | "down" -> (hPos, vPos + int instruction[1], 0)
+    | "up" -> (hPos, vPos - int instruction[1], 0)
+    | _ -> (hPos, vPos, 0)
+
+let solveDay2 positionUpdate (input:string[]) =
     input
     |> Array.map (fun s -> s.Split(" "))
-    |> Array.fold positionUpdate (0, 0) 
-    |> fun (vPos, hPos) -> vPos * hPos
+    |> Array.fold positionUpdate (0, 0, 0) 
+    |> fun (vPos, hPos, _) -> vPos * hPos
 
 getTestInput 2
-|> solveDay2
+|> solveDay2 positionUpdatePart2
 |> printfn "Test result: %d"
 
 getInput 2
-|> solveDay2
+|> solveDay2 positionUpdatePart2
 |> printfn "Result: %d"
