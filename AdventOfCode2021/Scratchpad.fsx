120cc363c8be8734b3c606a18894befe5b1d3fb7
Mark Allibone <mark.allibone@rey-technology.com>
2021-12-02 09:00:30 +0100

    Solves part one of day two

--- AdventOfCode2021/Scratchpad.fsx
+++ AdventOfCode2021/Scratchpad.fsx
@@ -10,3 +10,23 @@ let getInput day =
     File.ReadAllLines(filename day)
     
 
+let positionUpdate (hPos, vPos) (instruction:string[]) =
+    match instruction[0] with
+    | "forward" -> (hPos + int instruction[1], vPos)
+    | "down" -> (hPos, vPos + int instruction[1])
+    | "up" -> (hPos, vPos - int instruction[1])
+    | _ -> (hPos, vPos)
+
+let solveDay2 (input:string[]) =
+    input
+    |> Array.map (fun s -> s.Split(" "))
+    |> Array.fold positionUpdate (0, 0) 
+    |> fun (vPos, hPos) -> vPos * hPos
+
+getTestInput 2
+|> solveDay2
+|> printfn "Test result: %d"
+
+getInput 2
+|> solveDay2
+|> printfn "Result: %d"
