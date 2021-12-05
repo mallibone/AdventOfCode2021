99486cc8e4700646b1be4ed6966af8e818e8d5ac
Mark Allibone <mark.allibone@rey-technology.com>
2021-12-05 10:44:11 +0100

    Solves part 1 day 5

--- AdventOfCode2021/Scratchpad.fsx
+++ AdventOfCode2021/Scratchpad.fsx
@@ -10,76 +10,41 @@ let getInput day =
     let filename day = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day}.txt")
     File.ReadAllLines(filename day)
 
-let pivot input =
-    input
-    |> Seq.collect Seq.indexed // index the input
-    |> Seq.groupBy fst //group by index
-    |> Seq.map(snd >> Seq.map snd)
+let parseInput (inputLines:array<string>) =
+    inputLines
+    // parse input lines
+    |> Array.map (fun s -> s.Split(" -> ") |> Array.map(fun ip -> ip.Split(",") |> Array.map int )|> array2D)
+    // filter only horizontal and vertical lines
+    |> Array.filter(fun coords -> coords[0,0] = coords[1,0] || coords[0,1] = coords[1,1])
 
-let rec parseBingofields (input:seq<string>) (bingoFields:seq<seq<seq<string*int>>>) =
-    let getSkip collection = (Math.Min(6, (collection |> Seq.length)))
-    match Seq.isEmpty input with
-    | true -> bingoFields
-    | false ->
-        let newBingoField = input |> Seq.take 5 |> Seq.map (fun s -> s.Split(" ") |> Seq.filter (fun s -> not (String.IsNullOrWhiteSpace(s))) |> Seq.map(fun s -> (s,0)))
-        let bingoFields = if Seq.isEmpty bingoFields  then seq { newBingoField } else (Seq.append bingoFields (seq {newBingoField}))
-        printfn "input left: %A" input
-        parseBingofields (input |> Seq.skip (getSkip input)) bingoFields
+let createMap (lines:int[,][]) =
+    let maxY = ((lines |> Array.collect (fun coords -> [|(coords[0,0]);(coords[1,0])|])) |> Array.max) + 1
+    let maxX = ((lines |> Array.collect (fun coords -> [|(coords[0,1]);(coords[1,1])|])) |> Array.max) + 1
+    Array2D.create maxX maxY 0
 
-let isBingo bingoBoardFields =
-    bingoBoardFields |> Seq.exists (Seq.forall (fun x -> (snd x) = 1))
+let drawLine (map:int[,]) (line:int[,]) =
+    let ycoords = if line[0,0] < line[1,0] then [line[0,0] .. line[1,0]] else [line[0,0] .. -1 .. line[1,0]]
+    let xcoords = if line[0,1] < line[1,1] then [line[0,1] .. line[1,1]] else [line[0,1] .. -1 .. line[1,1]]
+    // printfn $"line: %A{line}"
+    // printfn $"xcoords: %A{xcoords} ycoords: %A{ycoords}"
+    for y in ycoords do
+        for x in xcoords do
+            map[x,y] <- map[x,y] + 1
+    map
 
-let sumUnmarkedBingoFields fields =
-    fields |> Seq.sumBy (fun bingoFields -> bingoFields |> Seq.collect(fun bingoRowFields -> bingoRowFields |> Seq.filter(fun field -> (snd field) = 0)) |> Seq.sumBy(fun field -> int (fst field)))
+let drawMap (lines:int[,][]) =
+    let initialMap = createMap lines
+    lines
+    |> Array.fold drawLine initialMap
 
-let playBingoRound roundNumber bingoFields =
-    bingoFields
-    |> Seq.map (fun field -> field |> Seq.map (fun lineItems -> lineItems |> Seq.map(fun e -> if fst e = roundNumber then (fst e, (snd e) + 1) else (fst e, snd e))))
+getTestInput 5
+|> parseInput
+|> drawMap
+|> Seq.cast<int> 
+|> Seq.sumBy (fun p -> if p > 1 then 1 else 0)
 
-let rec playBingoPart1 (numbers:list<string>) bingoFields =
-    match numbers with
-    | [] -> -1
-    | head::tail -> 
-        let newBingoFields = playBingoRound head bingoFields
-        // check for bingo
-        let havingBingoRow = (newBingoFields |> Seq.filter isBingo) |> Seq.append (newBingoFields |> Seq.map pivot |> Seq.filter isBingo)
-        // check for bingo
-        if (havingBingoRow |> Seq.length) > 0 then
-            let notCalledSum:int = sumUnmarkedBingoFields havingBingoRow
-            printfn $"row bingo sum %d{notCalledSum} last number %d{int head} "
-            notCalledSum * (int head)
-        else playBingoPart1 tail newBingoFields
-
-let rec playBingoPart2 (numbers:list<string>) bingoFields =
-    match numbers with
-    | [] -> -1
-    | head::tail -> 
-        let newBingoFields = 
-            playBingoRound head bingoFields
-            |> Seq.filter (fun x -> not (isBingo x))
-            |> Seq.filter (fun x -> x |> pivot |> isBingo |> not )
-        // check for bingo
-        // let havingBingoRow = (newBingoFields |> isBingo) |> Seq.append (newBingoFields |> Seq.map pivot |> isBingo)
-        // check for bingo
-        if newBingoFields |> Seq.length = 1 then
-            // printfn "last number %A" head
-            // printfn "remaining fields %A" (newBingoFields |> Seq.map (Seq.map Seq.toList) |> Seq.map Seq.toList |> Seq.toList)
-            playBingoPart1 tail newBingoFields
-        else
-            // printfn "last number %A" head
-            // printfn "remaining fields %A" (newBingoFields |> Seq.map (Seq.map Seq.toList) |> Seq.map Seq.toList |> Seq.toList)
-            playBingoPart2 tail newBingoFields
-
-let testInput = getTestInput 4
-let testNumbers = testInput |> Seq.head |> fun s -> s.Split(",") |> Seq.toList
-let testBingoFields = parseBingofields (testInput |> Array.skip 2) [||]
-
-playBingoPart1 testNumbers testBingoFields |> printfn "Bingo Baby: %d"
-playBingoPart2 testNumbers testBingoFields |> printfn "Bingo Baby: %d"
-
-let input = getInput 4
-let numbers = input |> Seq.head |> fun s -> s.Split(",") |> Seq.toList
-let bingoFields = parseBingofields (input |> Array.skip 2) [||]
-
-playBingoPart1 numbers bingoFields |> printfn "Bingo Baby: %d"
-playBingoPart2 numbers bingoFields |> printfn "Bingo Baby: %d"
+getInput 5
+|> parseInput
+|> drawMap
+|> Seq.cast<int> 
+|> Seq.sumBy (fun p -> if p > 1 then 1 else 0)
