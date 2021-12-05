f2780ce476a513f3da63f8b446123abd83311bb0
Mark Allibone <mark.allibone@rey-technology.com>
2021-12-05 18:21:19 +0100

    Solves day 5 part 2

--- AdventOfCode2021/Scratchpad.fsx
+++ AdventOfCode2021/Scratchpad.fsx
@@ -10,41 +10,72 @@ let getInput day =
     let filename day = Path.Combine(__SOURCE_DIRECTORY__, $"Input/Day{day}.txt")
     File.ReadAllLines(filename day)
 
-let parseInput (inputLines:array<string>) =
+let isHorizontalLine (coords:int[,]) = coords[0,0] = coords[1,0]
+let isVerticalLine (coords:int[,]) = coords[0,1] = coords[1,1]
+let isDiagonalLine (coords:int[,]) = Math.Abs(coords[0,0] - coords[1,0]) = Math.Abs(coords[0,1] - coords[1,1])
+    // printfn "%A" coords
+    // let isDiagonalLine = Math.Abs(coords[0,0] - coords[1,0]) = Math.Abs(coords[0,1] - coords[1,1])
+    // printfn "%A" isDiagonalLine
+    // isDiagonalLine
+
+let toVector (lineCoords:int[,]) =
+    match lineCoords with
+    | c when isHorizontalLine c -> 
+        if lineCoords[0,1] < lineCoords[1,1] then [|lineCoords[0,1] .. lineCoords[1,1]|] else [|lineCoords[0,1] .. -1 .. lineCoords[1,1]|]
+        |> Array.map (fun x -> [|lineCoords[0,0];x|])
+    | c when isVerticalLine c ->
+        if lineCoords[0,0] < lineCoords[1,0] then [|lineCoords[0,0] .. lineCoords[1,0]|] else [|lineCoords[0,0] .. -1 .. lineCoords[1,0]|]
+        |> Array.map (fun y -> [|y;lineCoords[0,1]|])
+    | c when isDiagonalLine c ->
+        let points = Math.Abs(lineCoords[0,0] - lineCoords[1,0])
+        let diagLine =
+            let xinvert = if lineCoords[0,0] - lineCoords[1,0] < 0 then 1 else -1
+            let yinvert = if lineCoords[0,1] - lineCoords[1,1] < 0 then 1 else -1
+            [|0 .. points|] |> Array.map (fun i -> [|lineCoords[0,0]+(i*xinvert);lineCoords[0,1]+(i*yinvert)|])
+        // printfn "diagline: %A" diagLine
+        diagLine
+    | _ -> [||]
+    |> array2D
+
+let parseInput filterRules (inputLines:array<string>) =
     inputLines
-    // parse input lines
     |> Array.map (fun s -> s.Split(" -> ") |> Array.map(fun ip -> ip.Split(",") |> Array.map int )|> array2D)
-    // filter only horizontal and vertical lines
-    |> Array.filter(fun coords -> coords[0,0] = coords[1,0] || coords[0,1] = coords[1,1])
+    |> Array.filter filterRules
+    |> Array.map toVector
 
 let createMap (lines:int[,][]) =
-    let maxY = ((lines |> Array.collect (fun coords -> [|(coords[0,0]);(coords[1,0])|])) |> Array.max) + 1
-    let maxX = ((lines |> Array.collect (fun coords -> [|(coords[0,1]);(coords[1,1])|])) |> Array.max) + 1
-    Array2D.create maxX maxY 0
+    let maxY = (((lines |> Array.collect (fun coords -> [|for i in [0 .. (coords |> Array2D.length1) - 1] do (coords[i,0]);(coords[i,0])|])) |> Array.max) + 1)
+    let maxX = (((lines |> Array.collect (fun coords -> [|for i in [0 .. (coords |> Array2D.length1) - 1] do (coords[i,1]);(coords[i,1])|])) |> Array.max) + 1)
+    printfn $"Max X. {maxX} Max Y: {maxY}"
+    Array2D.create maxY maxX 0
 
-let drawLine (map:int[,]) (line:int[,]) =
-    let ycoords = if line[0,0] < line[1,0] then [line[0,0] .. line[1,0]] else [line[0,0] .. -1 .. line[1,0]]
-    let xcoords = if line[0,1] < line[1,1] then [line[0,1] .. line[1,1]] else [line[0,1] .. -1 .. line[1,1]]
-    // printfn $"line: %A{line}"
-    // printfn $"xcoords: %A{xcoords} ycoords: %A{ycoords}"
-    for y in ycoords do
-        for x in xcoords do
-            map[x,y] <- map[x,y] + 1
+let drawLine (map:int[,]) (coords:int[,]) =
+    // printfn " coords %A" coords
+    // printfn "x: %A y: %A" ((coords |> Array2D.length1) - 1) ((coords |> Array2D.length2) - 1)
+    // if ((coords |> Array2D.length1) - 1) = 652 then printfn "last coords: %A" coords 
+
+    for x in [0 .. (coords |> Array2D.length1) - 1] do
+        map[coords[x,0],coords[x,1]] <- map[coords[x,0],coords[x,1]] + 1
     map
 
 let drawMap (lines:int[,][]) =
     let initialMap = createMap lines
-    lines
-    |> Array.fold drawLine initialMap
+    let map = 
+        lines
+        |> Array.fold drawLine initialMap
+    // printfn $"%A{map}" 
+    map
 
 getTestInput 5
-|> parseInput
+// |> parseInput (fun i -> isHorizontalLine i || isVerticalLine i)
+|> parseInput (fun i -> isHorizontalLine i || isVerticalLine i || isDiagonalLine i)
 |> drawMap
 |> Seq.cast<int> 
 |> Seq.sumBy (fun p -> if p > 1 then 1 else 0)
 
 getInput 5
-|> parseInput
+// |> parseInput (fun i -> isHorizontalLine i || isVerticalLine i)
+|> parseInput (fun i -> isHorizontalLine i || isVerticalLine i || isDiagonalLine i)
 |> drawMap
 |> Seq.cast<int> 
 |> Seq.sumBy (fun p -> if p > 1 then 1 else 0)
